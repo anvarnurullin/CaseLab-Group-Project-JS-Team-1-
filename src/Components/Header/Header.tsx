04514d10780717ed44f80fc503e1c1b2bd58e0fe
dilnabz <artikovadilnoza19@gmail.com>
2022-02-23 15:08:53 +0300

    Create Header

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./logo.png";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="Header">
+      <img src={logo} alt="Здесь логтип" />
+      <div className="navigation">
+        <button>Главная</button>
+        <button>Меню</button>
+        <button>Контакты</button>
+      </div>
+      <div className="contactsHeader">
+        <p>Наш телефон</p>
+        <p>
+          <a>+996 705 188 955</a>
+          <a>+996 555 188 955</a>
+        </p>
+        <p>работаем с 10:00 до 00:00</p>
+      </div>
+      <div className="cartIcon"></div>
+    </div>
+  );
+}
+
+export default Header;
