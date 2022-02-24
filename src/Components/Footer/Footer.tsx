da14e15bfa2cd47bbdfcf22975259f8ab743996a
dilnabz <artikovadilnoza19@gmail.com>
2022-02-24 10:33:09 +0300

    Create Header and Footer

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <section className="Footer">
+      <ul className="footerContacts">
+        <li>Тел.: +996 705 188 955</li>
+        <li>Тел.: +996 555 188 955</li>
+        <li>Адрес: Комсомольский просп., 28</li>
+        <li>
+          <a href="#">Авторизация для сотрудников</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Footer;
