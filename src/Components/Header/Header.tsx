02d1eec92175b39df504f1934ad338654006ba89
dilnabz <artikovadilnoza19@gmail.com>
2022-02-26 18:17:41 +0300

    Changed fontSize and CartStyle resolving conflicts

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -71,9 +71,9 @@ function Header() {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                height="30px"
+                height="50px"
                 viewBox="0 0 24 24"
-                width="30px"
+                width="50px"
                 fill="#000000"
               >
                 <path d="M0 0h24v24H0V0z" fill="none" />
