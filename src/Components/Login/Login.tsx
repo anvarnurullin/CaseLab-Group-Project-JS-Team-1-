ce12b2a95f4ef4f10cf7f90d41a251cc0d825705
maxzh22 <98665133+maxzh22@users.noreply.github.com>
2022-02-28 21:51:54 +0300

    Create Login

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -4,61 +4,57 @@ import { useDispatch } from "react-redux";
 import { setMenuAction } from "../../store/menuReducer";
 import { useEffect, useRef } from "react";
 
-function Cart() {
+function Login() {
   const dispatch = useDispatch();
 
-  const modal_backdrop = useRef<HTMLDivElement>(null);
+  const modal_auth = useRef<HTMLDivElement>(null);
   const form = useRef<HTMLFormElement>(null);
 
   useEffect(() => {
-    modal_backdrop.current?.addEventListener("click", (e) => {
-      if (e.target == modal_backdrop.current) dispatch(setMenuAction("main"));
-    });
-    form.current?.addEventListener("submit", (e) => {
-      e.preventDefault();
-      if (modal_backdrop.current) {
-        modal_backdrop.current.style.removeProperty("display");
-        console.log(Object.fromEntries(new FormData(form.current!))); // значения формы для запроса к бэкенду
-      }
-    });
+    if (modal_auth.current && form.current) {
+      modal_auth.current.onclick = event => {
+        if (event.target === modal_auth.current)
+          dispatch(setMenuAction("main"))
+      };
+      form.current.onsubmit = (event) => {
+        event.preventDefault();
+        modal_auth.current?.style.removeProperty("display");
+        console.log(Object.fromEntries(new FormData(form.current!))); 
+      };
+      form.current.querySelectorAll('input').forEach(input => Object.assign(input, {
+        oninvalid(this: HTMLInputElement, event) {
+          this.setCustomValidity('');
+          if (!this.validity.valid && this.dataset.validationError)
+            this.setCustomValidity(this.dataset.validationError);
+        },
+        oninput(this: HTMLInputElement, event) {
+          this.setCustomValidity('');
+        }
+      } as GlobalEventHandlers));
+    }
   }, []);
 
-  return (
-    <>
-      <div className="Login">
-        <form className="wrap" ref={form}>
-          <fieldset className="fields">
-            <h2>Авторизация</h2>
-            <fieldset className="login">
-              <label htmlFor="login" />
-              <input type="text" placeholder="Логин" id="login" name="login" />
+  return <>
+    <div className="Login">
+      <form className="wrap" ref={form}>
+        <fieldset className="field_login">
+          <h2>Авторизация</h2>
+          <fieldset>
+            <input
+              type="text" id="login" name="login" placeholder="Логин"
+              required={true} pattern="[a-zA-Zа-яА-Я]+" data-validation-error="Только киррилица"/>
             </fieldset>
-            <fieldset className="password">
-              <label htmlFor="password" />
-              <input
-                type="password"
-                placeholder="Пароль"
-                id="password"
-                name="password"
-              />
-            </fieldset>
-            <button type="submit" className="submitBtn">
-              <h3>Оформить заказ</h3>
-            </button>
+          <fieldset className="field_password">
+            <input
+              type="password" id="password" name="password" placeholder="Пароль"
+              required={true} pattern="[a-zA-Zа-яА-Я]+" data-validation-error="Только киррилица"/>
           </fieldset>
-        </form>
-      </div>
-      <div
-        className="Modal cart_modal"
-        style={{ display: "none" }}
-        ref={modal_backdrop}
-      >
-        <div className="modalCard">
-          <h2>Спасибо за заказ!</h2>
-        </div>
-      </div>
-    </>
-  );
+          <button className="loginBtn">Войти</button>
+        </fieldset>
+      </form>
+    </div>
+    
+  </>;
 }
 
-export default Cart;
+export default Login;
